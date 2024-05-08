2c70c6487a6cd7202bfe7970aeee84c612f72e65
Jefferson <stupidjeff430@gmail.com>
2024-05-08 08:43:48 +0700

    fix: date again

--- update-readme.js
+++ update-readme.js
@@ -87,7 +87,7 @@ async function fetchGitHubData() {
 <h3>Hi 👋, My name is ${userData.name || username}</h3>
 
 > 
-    Based in Indonesia and it's supposed to be ${weatherData.temperature}°C (${weatherData.temperatureF}°F) and ${weatherData.weatherEmoji} ${weatherData.weatherText} today. Have a great ${new Date().toLocaleDateString('en-US', { weekday: 'long', timeZone: 'Asia/Jakarta'  })}!
+    Based in Indonesia and it's supposed to be ${weatherData.temperature}°C (${weatherData.temperatureF}°F) and ${weatherData.weatherEmoji} ${weatherData.weatherText} today. Have a great ${new Date().toLocaleDateString('en-US', { weekday: 'long'})}!
 
 I'm 22 year old Self Taught Developer based in ${userData.location || 'Not specified'} and have a deep passion for web development.
 
